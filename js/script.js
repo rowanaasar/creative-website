ddd27651b861340666af8311350e39a726d40bbe
rowanaasar <54355801+rowanaasar@users.noreply.github.com>
2021-03-17 00:56:09 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.single-item').slick({
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        fade: true,
+        fadeSpeed: 200
+    });
+});
